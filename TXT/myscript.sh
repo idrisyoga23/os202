62fa98444b94d34bafeeda097ad8d656968a8aee
idrisyoga23 <idrisyoga24@gmail.com>
2020-10-04 10:30:00 +0700

    Os202 idrisyoga23 delete some file and push again

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,4 +1,19 @@
 
+
+#!/bin/bash
+echo "Hello World"
+echo "Enter Your Name"
+read name
+echo "Welcome $name to Os202"
+#!/bin/bash
+for (( counter=10; counter>0; counter-- ))
+do
+echo -n "$counter "
+done
+printf "\n"
+
+
+
 #!/bin/bash
 valid=true
 count=1
@@ -12,18 +27,18 @@ fi
 ((count++))
 done
 
-
 #!/bin/bash
-echo "Hello World"
-
-#!/bin/bash
-for (( counter=10; counter>0; counter-- ))
-do
-echo -n "$counter "
-done
-printf "\n"
-
-echo "Enter Your Name"
-read name
-echo "Welcome $name to LinuxHint"
-
+echo "Enter a valid number"
+read n
+if [ $n -eq 101 ];
+then
+echo "This is first number"
+elif [ $n -eq 510 ];
+then
+echo " This is second number "
+elif [ $n -eq 999 ];
+then
+echo " This is third number "
+else
+echo "No numbers over here"
+fi
